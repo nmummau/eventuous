44d86e7bfbb0e70168bb5d2a85d1e40cb43d19db
Neal Mummau <nmummau@webstaurantstore.com>
2025-09-22 11:39:19 -0400

    feat(mssql): Minor SQL changes; no functional changes - SET NOCOUNT ON - remove unused declared variables - specify all parameters when calling [check_stream] procedure - formatting: each column on it's own line. Best practice for maintainability of procedures - proper casing of keywords (IF, ISNULL, NVARCHAR) - use alias to UPDATE statement - remove 'AS' keyword - semicolons

--- src/SqlServer/src/Eventuous.SqlServer/Scripts/2_AppendEvents.sql
+++ src/SqlServer/src/Eventuous.SqlServer/Scripts/2_AppendEvents.sql
@@ -5,46 +5,74 @@ CREATE OR ALTER PROCEDURE __schema__.append_events
     @messages __schema__.StreamMessage READONLY
 AS
 BEGIN
-    DECLARE @current_version INT,
+    SET NOCOUNT ON;
+
+    DECLARE
+        @current_version INT,
         @stream_id INT,
         @position BIGINT,
-        @customErrorMessage NVARCHAR(200),
-        @newMessagesCount INT,
-        @expected_StreamVersionAfterUpdate INT,
-        @actual_StreamVersionAfterUpdate INT
+        @customErrorMessage NVARCHAR(200);
 
-    if @created is null
+    IF @created IS NULL
         BEGIN
-            SET @created = SYSUTCDATETIME()
+            SET @created = SYSUTCDATETIME();
         END
 
-    EXEC [__schema__].[check_stream] @stream_name, @expected_version, @current_version = @current_version OUTPUT, @stream_id = @stream_id OUTPUT
+    EXEC [__schema__].check_stream
+        @stream_name      = @stream_name,
+        @expected_version = @expected_version,
+        @current_version  = @current_version OUTPUT,
+        @stream_id        = @stream_id OUTPUT;
 
     BEGIN TRY
-        INSERT INTO __schema__.Messages (MessageId, MessageType, StreamId, StreamPosition, JsonData, JsonMetadata, Created)
-        SELECT message_id, message_type, @stream_id, @current_version + (ROW_NUMBER() OVER(ORDER BY (SELECT NULL))), json_data, json_metadata, @created
+        INSERT INTO __schema__.Messages (
+            MessageId,
+            MessageType,
+            StreamId,
+            StreamPosition,
+            JsonData,
+            JsonMetadata,
+            Created
+        )
+        SELECT
+            message_id,
+            message_type,
+            @stream_id,
+            @current_version + (ROW_NUMBER() OVER(ORDER BY (SELECT NULL))),
+            json_data,
+            json_metadata,
+            @created
         FROM @messages
     END TRY
     BEGIN CATCH
         IF (ERROR_NUMBER() = 2627 OR ERROR_NUMBER() = 2601) AND (SELECT CHARINDEX(N'UQ_StreamIdAndStreamPosition', ERROR_MESSAGE())) > 0
             BEGIN
-                DECLARE @streamIdFromError nvarchar(20) = SUBSTRING(ERROR_MESSAGE(), PATINDEX(N'%[0-9]%,%', ERROR_MESSAGE()), PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE()) - PATINDEX(N'%[0-9]%,%', ERROR_MESSAGE()))
-                DECLARE @streamPositionFromError nvarchar(20) = SUBSTRING(ERROR_MESSAGE(), (PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE())) + 2, PATINDEX(N'%).', ERROR_MESSAGE()) - (PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE()) + 2))
+                DECLARE @streamIdFromError NVARCHAR(20) = SUBSTRING(ERROR_MESSAGE(), PATINDEX(N'%[0-9]%,%', ERROR_MESSAGE()), PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE()) - PATINDEX(N'%[0-9]%,%', ERROR_MESSAGE()))
+                DECLARE @streamPositionFromError NVARCHAR(20) = SUBSTRING(ERROR_MESSAGE(), (PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE())) + 2, PATINDEX(N'%).', ERROR_MESSAGE()) - (PATINDEX(N'%, [0-9]%).', ERROR_MESSAGE()) + 2))
 
                 -- TODO: There are multiple causes of OptimisticConcurrencyExceptions, but current client code is hard-coded to check for 'WrongExpectedVersion' in message and 50000 as error number.
                 SELECT @customErrorMessage = FORMATMESSAGE(N'WrongExpectedVersion, another message has already been written at stream position %s on stream %s.', @streamIdFromError, @streamPositionFromError);
                 THROW 50000, @customErrorMessage, 1;
-            END
+            END;
         ELSE
-            THROW
-    END CATCH
-    
-    SELECT TOP 1 @current_version =  StreamPosition, @position = GlobalPosition
-    FROM __schema__.Messages
+        BEGIN
+            ;THROW;
+        END;
+    END CATCH;
+
+    SELECT TOP (1)
+        @current_version = StreamPosition,
+        @position = GlobalPosition
+    FROM __schema__.[Messages]
     WHERE StreamId = @stream_id
-    ORDER BY GlobalPosition DESC
+    ORDER BY GlobalPosition DESC;
 
-    UPDATE __schema__.Streams SET Version = @current_version WHERE StreamId = @stream_id
+    UPDATE s
+    SET [Version] = @current_version
+    FROM __schema__.Streams s
+    WHERE s.StreamId = @stream_id;
 
-    SELECT @current_version AS current_version, @position AS position
-END
+    SELECT
+        @current_version current_version,
+        @position position;
+END;
