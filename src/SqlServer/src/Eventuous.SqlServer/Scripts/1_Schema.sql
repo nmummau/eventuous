0c79814508f8360bc5690b4179bc69965375a74b
Neal Mummau <nmummau@webstaurantstore.com>
2025-09-22 11:38:06 -0400

    Minor SQL updates - consistent casing of [Version] column - explicitly specify NULLable columns

--- src/SqlServer/src/Eventuous.SqlServer/Scripts/1_Schema.sql
+++ src/SqlServer/src/Eventuous.SqlServer/Scripts/1_Schema.sql
@@ -12,7 +12,7 @@ IF OBJECT_ID('__schema__.Streams', 'U') IS NULL
             [Version]  INT DEFAULT (-1)   NOT NULL,
             CONSTRAINT PK_Streams PRIMARY KEY CLUSTERED (StreamId),
             CONSTRAINT UQ_StreamName UNIQUE NONCLUSTERED (StreamName),
-            CONSTRAINT CK_VersionGteNegativeOne CHECK ([version] >= -1)
+            CONSTRAINT CK_VersionGteNegativeOne CHECK ([Version] >= -1)
         );
     END
 
@@ -25,9 +25,9 @@ IF OBJECT_ID('__schema__.Messages', 'U') IS NULL
             StreamId       INT                   NOT NULL,
             StreamPosition INT                   NOT NULL,
             GlobalPosition BIGINT IDENTITY (0,1) NOT NULL,
-            JsonData       NVARCHAR(max)         NOT NULL,
-            JsonMetadata   NVARCHAR(max),
-            Created        DATETIME2,
+            JsonData       NVARCHAR(MAX)         NOT NULL,
+            JsonMetadata   NVARCHAR(MAX)             NULL,
+            Created        DATETIME2                 NULL,
             CONSTRAINT PK_Events PRIMARY KEY CLUSTERED (GlobalPosition),
             CONSTRAINT FK_MessageStreamId FOREIGN KEY (StreamId) REFERENCES __schema__.Streams (StreamId),
             CONSTRAINT UQ_StreamIdAndStreamPosition UNIQUE NONCLUSTERED (StreamId, StreamPosition),
@@ -42,7 +42,7 @@ IF OBJECT_ID('__schema__.Checkpoints', 'U') IS NULL
         CREATE TABLE __schema__.Checkpoints
         (
             Id       NVARCHAR(128) NOT NULL,
-            Position BIGINT        NULL,
+            Position BIGINT            NULL,
             CONSTRAINT PK_Checkpoints PRIMARY KEY CLUSTERED (Id),
         );
     END
@@ -53,8 +53,8 @@ IF TYPE_ID('__schema__.StreamMessage') IS NULL
         (
             message_id    UNIQUEIDENTIFIER NOT NULL,
             message_type  NVARCHAR(128)    NOT NULL,
-            json_data     NVARCHAR(max)    NOT NULL,
-            json_metadata NVARCHAR(max)
+            json_data     NVARCHAR(MAX)    NOT NULL,
+            json_metadata NVARCHAR(MAX)        NULL
         )
     END
 
